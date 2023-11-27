d41e824af64b3f911b79e493c5d0342bc8318499
Woetoe05 <144440667+Woetoe05@users.noreply.github.com>
2023-11-27 21:46:36 +0100

    Update main.cpp

--- EXPO20-main/EXPO20-main/test stepper/src/main.cpp
+++ EXPO20-main/EXPO20-main/test stepper/src/main.cpp
@@ -1,13 +1,7 @@
 //ready not safe STATE
 #include <Arduino.h>
-#include <Servo.h>
 #include <AccelStepper.h>
 using namespace std;
-
-//Servo's
-Servo ServoScrew;
-Servo ServoValve;
-
 
 //Arm define
 int moveDir = 1;
@@ -18,7 +12,7 @@ const float degPerStep = (360.0/6400.0);
 const int dirPin = 7;
 const int stepPin = 6;
 #define stepperEnable 26
-#define motorInterfaceType 1
+#define motorInterfaceType 1 
 AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
 bool stopped = false;
 
@@ -91,17 +85,12 @@ void setup() {
   pinMode(stepperEnable, OUTPUT);   
   digitalWrite(stepperEnable, LOW);
   Serial.begin(9600);
-  //close all servos-->
-  //calibrate sensors-->
-  //stepper motor still-->
-  //ready safe STATE-->
+
 }
 
 void loop() {
   wantedAmount = Serial.read();
   minimumCE = 0.98 *wantedAmount;
-  //dispensing STATE
-  //check if there is a bag | not necesserry for our project-->
   while (currentAmount < minimumCE){
     currentAmount = checkCurrentAmount(currentAmount);
     int theState = whichState(currentAmount, wantedAmount, minimumCE);

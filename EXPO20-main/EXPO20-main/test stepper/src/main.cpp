670e9b520713129445d9b4a4e118dd63c9e71f93
Woetoe05 <144440667+Woetoe05@users.noreply.github.com>
2023-12-19 13:23:53 +0100

    Update main.cpp
    working without library from zakaria

--- EXPO20-main/EXPO20-main/test stepper/src/main.cpp
+++ EXPO20-main/EXPO20-main/test stepper/src/main.cpp
@@ -1,108 +1,55 @@
-//ready not safe STATE
 #include <Arduino.h>
-#include <AccelStepper.h>
-using namespace std;
 
-//Arm define
-int moveDir = 1;
-const float stepsPerDeg = (6400.0/360.0); // 6400 = 360 deg
-const float degPerStep = (360.0/6400.0);
+#define dirPin 7
+#define stepPin 6
+const int stepsPerRevolution = 200;
+const int rotationSpeed = 10000;
+const int angle = 180;
 
-//Stepper motor
-const int dirPin = 7;
-const int stepPin = 6;
-#define stepperEnable 26
-#define motorInterfaceType 1 
-AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
-bool stopped = false;
-
-// constants
-int stepsDone = 0;
-int normalSpeed = 50;
-int slowSpeed = 10;
-int stopSpeed = 0;
-
-// variabels
-double wantedAmount;
-double currentAmount = 0;
-double minimumCE;
-//code-------------------------------------------------------------|
-
-//Alles van Arm----------------------------------------------------|
-void runSpeed(int neededSpeed){
-  myStepper.setSpeed(normalSpeed);
-  myStepper.runSpeed();
+long rotate(long degrees)
+{
+    int steps = map(degrees, 0, 360, 0, 200);
+    return steps;    
 }
-void switchSpeed(int theState){
-  switch (theState)
+
+void stepperForward()
+{
+    // Set motor direction clockwise
+    digitalWrite(dirPin, HIGH);
+ 
+    // Spin motor
+    for(int x = 0; x < rotate(angle); x++)
     {
-    case 10:
-      runSpeed(normalSpeed);
-      break;
-    case 20:
-      runSpeed(slowSpeed);
-      break;
-    case 30:
-      runSpeed(stopSpeed);
-      //close all servo's-->
-      break;
-    case 0:
-      //emergency stop
-      break;
-    default:
-      break;
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(rotationSpeed);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(rotationSpeed);
     }
-    
 }
-//Alles van weight-------------------------------------------------|
-int checkCurrentAmount(double currentAmount){
-  // for now it is +=10
-  // later it will check the weight
-  currentAmount += 10;
-  return currentAmount;
-}
-int whichState(double currentAmount, double wantedAmount, double minimumCE){
-  if (currentAmount <= 0.8*wantedAmount){//normal speed under risk amount
-    return 10;
-  }
-  else if (currentAmount >= 0.8*wantedAmount){//slower speed above risk amount
-    return 20;
-  }
-  else if (currentAmount >= minimumCE){//stop when in CE range
-    return 30;
-  }
-  else{
-    return 0;
+
+void stepperBackward()
+{
+    // Set motor direction counterclockwise
+    digitalWrite(dirPin, LOW);
+ 
+    // Spin motor
+    for(int x = 0; x < rotate(angle); x++)
+    {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(rotationSpeed);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(rotationSpeed);  
     }
 }
 
 void setup() {
-   //Arm void setup----------------------------------------------------------|
-  delay(1000);
-  myStepper.setMaxSpeed(1000);
-  myStepper.setAcceleration(1000);
-  myStepper.setCurrentPosition(0);
-  pinMode(stepperEnable, OUTPUT);   
-  digitalWrite(stepperEnable, LOW);
-  Serial.begin(9600);
-
+    pinMode(stepPin, OUTPUT);
+    pinMode(dirPin, OUTPUT);
+    Serial.begin(9600);
 }
 
-void loop() {
-  wantedAmount = Serial.read();
-  minimumCE = 0.98 *wantedAmount;
-  while (currentAmount < minimumCE){
-    currentAmount = checkCurrentAmount(currentAmount);
-    int theState = whichState(currentAmount, wantedAmount, minimumCE);
-    switchSpeed(theState);
-    if (theState == 30){
-      //close all servo's
-      //finished state
-      break;
-    }
-    stepsDone = myStepper.currentPosition();
-  }
-  Serial.print("This is the amount you got dispensed: ");
-  Serial.println(currentAmount);
-
+void loop() { 
+    // Your loop code here, including calls to stepperForward, stepperBackward, etc.
+    stepperForward();
+    delay(100);
 }

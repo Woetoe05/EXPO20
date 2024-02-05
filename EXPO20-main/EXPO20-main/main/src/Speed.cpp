14933c074e64bd3e7e018c1bba80eb4e8aac62a9
Woetoe05 <144440667+Woetoe05@users.noreply.github.com>
2024-02-05 22:34:27 +0100

    Update Speed.cpp

--- EXPO20-main/EXPO20-main/main/src/Speed.cpp
+++ EXPO20-main/EXPO20-main/main/src/Speed.cpp
@@ -1,36 +1,4 @@
 #include <Calculations.h>
-//#include <AccelStepper.h>
-//#include <main.cpp>
-//int normalSpeed = 50;
-//int slowSpeed = 10;
-//int stopSpeed = 0;
-//
-//void runSpeed(int neededSpeed){
-//  myStepper.setSpeed(neededSpeed);
-//  myStepper.runSpeed();
-//}
-//
-//void switchSpeed(int theState){
-//  switch (theState)
-//    {
-//    case 10:
-//      runSpeed(normalSpeed);
-//      break;
-//    case 20:
-//      runSpeed(slowSpeed);
-//      break;
-//    case 30:
-//      runSpeed(stopSpeed);
-//      //close all servo's-->
-//      break;
-//    case 0:
-//      //emergency stop
-//      break;
-//    default:
-//      break;
-//    }
-//    
-//}
 #include <Arduino.h>
 
 #define dirPin 7
